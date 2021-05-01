d7a76350ff1c5a7e416840545b31fe400049d28c
denizcamalan <79871039+denizcamalan@users.noreply.github.com>
2021-05-01 14:59:41 +0300

    update

---
+++ MyProject/src/test/java/testing/Activity1.java
@@ -0,0 +1,135 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
+import java.util.Random;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+
+public class Activity1 {
+
+		public static void main(String[] args) {
+			
+			// www.gittigidiyor.com sitesi açılır.
+				
+				WebDriver driver = new ChromeDriver();
+				driver.manage().window().setSize(new Dimension(1440, 875));
+				System.setProperty("webdriver.chrome.driver", "/Users/denizcamalan/Downloads/SeleniumPacks/chromedriver");
+				driver.get("https://www.gittigidiyor.com");
+				
+			// Ana sayfanın açıldığı kontrol edilir.
+				
+				String expected_title = "GittiGidiyor - Türkiye'nin Öncü Alışveriş Sitesi";
+				String actual_title1 = driver.getTitle();
+				
+				if (expected_title.equals(actual_title1)) {
+					System.out.println("Opened Site Succesful");
+				}else {
+					System.out.println("Failed");
+				}
+				
+			//  Siteye login olunur.
+				
+				driver.get("https://www.gittigidiyor.com/uye-girisi");
+				driver.findElement(By.id("L-UserNameField")).sendKeys("e-mail");
+				driver.findElement(By.id("L-PasswordField")).sendKeys("password");
+				driver.findElement(By.id("gg-login-enter")).click();
+				
+			// Login işlemi kontrol edilir.
+					
+				if (expected_title.equals(actual_title1)) {
+					System.out.println("Login Succesfull");
+				}else {
+					System.out.println("Login Failled");
+				} 
+				
+			// Arama kutucuğuna bilgisayar kelimesi girilir. 
+			
+			    WebElement searchbtn = driver.findElement(By.cssSelector(".psxmjs-2"));
+			    Actions builder = new Actions(driver);
+			    builder.moveToElement(searchbtn).perform();
+			    driver.findElement(By.name("k")).click();
+			    driver.findElement(By.name("k")).sendKeys("bilgisayar");
+			    driver.findElement(By.name("k")).sendKeys(Keys.ENTER);
+			    
+			// Arama sonuçları sayfasından 2.sayfa açılır. 
+
+			    JavascriptExecutor js = (JavascriptExecutor) driver; 
+			    WebElement tab2 = driver.findElement(By.xpath("//div[@id='best-match-right']/div[5]/ul/li[2]/a"));
+			    js.executeScript("arguments[0].click();", tab2);
+				String actual_title3 = driver.getTitle();
+				
+			// 2.sayfanın açıldığı kontrol edilir.
+			    
+			    String expected_title2 = "Bilgisayar - GittiGidiyor - 2/100";
+			    
+			    if (expected_title2.equals(actual_title3)) {
+					System.out.println("Opened 2 Succesful");
+				}else {
+					System.out.println("Opened 2 Failled");
+				} 
+			    
+			 // Sonuca göre sergilenen ürünlerden rastgele bir ürün seçilir.
+			    
+			   WebElement randomProd = driver.findElement(By.xpath("//div[@id='best-match-right']/div[3]/div[2]/ul"));
+			   List<WebElement> l1 = randomProd.findElements(By.tagName("a"));
+			   Random r = new Random();
+			   l1.get(r.nextInt(l1.size())).click();
+			   WebElement price = driver.findElement(By.xpath("//div[@id='sp-price-container']/div[4]"));
+			   String priceText= price.getText();      
+
+			 // Seçilen ürün sepete eklenir.
+			   
+			   WebElement addBasket = driver.findElement(By.xpath("//div[@id='sp-addbasket-button']/form/button"));
+			   js.executeScript("arguments[0].click();", addBasket);
+			   WebDriverWait wait = new WebDriverWait(driver, 100);
+			   wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='header_wrapper']/div[4]/div[3]/div/div/div/div[2]/div[4]/div/a"))).click();
+
+			 // Ürün sayfasındaki fiyat ile sepette yer alan ürün fiyatının doğruluğu karşılaştırılır.
+			    
+			   WebElement basketPrice = driver.findElement(By.cssSelector(".new-price"));
+		       String actual_price = basketPrice.getText();
+		       
+		       if(actual_price.compareTo(priceText)>0){
+		       	System.out.println("Price Equal");
+		       }else 
+		       System.out.println("Price Failed");
+		      
+		      // Adet arttırılarak ürün adedinin 2 olduğu doğrulanır.
+
+		       driver.findElement(By.cssSelector(".gg-input-select > .amount")).sendKeys("2");
+
+		       WebElement twoPrice = driver.findElement(By.xpath("//form[@id='submit-cart']/div/div[2]/div[3]/div/div/div/div[5]/div/div/ul/li/div"));
+		       String tot_price = twoPrice.getText();
+		       String expectedText = "Ürün Toplamı (2 Adet)";
+		       
+		       if (tot_price.compareTo(expectedText)>0) {
+					System.out.println("2 Product Succesful");
+		       }else {
+					System.out.println("2 Product Failled");
+				} 
+		       
+			  // Ürün sepetten silinerek sepetin boş olduğu kontrol edilir.
+		       
+			    driver.findElement(By.cssSelector(".row > .btn-delete > .gg-icon")).click(); 
+
+			    WebElement checkbasket = driver.findElement(By.xpath("//h2[contains(.,'Sepetinizde ürün bulunmamaktadır.')]"));
+			    String check_prod = checkbasket.getText();
+			    String actual_prod = "Sepetinizde ürün bulunmamaktadır.";
+			    System.out.println(check_prod);
+		       
+		        if(check_prod.compareTo(actual_prod)>0){
+				       System.out.println("Succesful");
+		        }else
+				       System.out.println("Failled");				 
+		}
+	
+}
